9fff7f97bdd8a482b2f993ee12abd28949469324
Apple <apple@Apples-MacBook-Pro.local>
2021-04-21 11:30:13 +0530

    clean code

--- controllers/events.js
+++ controllers/events.js
@@ -1,68 +1,48 @@
 const Event = require('../models/event')
 
-module.exports.eventListAll = async (req, res, next) => {
-    try {
-        const newEvent = await Event.find()
-        console.log(newEvent)
-        res.send(newEvent)
-    } catch(err) {
-        res.send(err)
-    }
+module.exports.eventListAll = () => {
+    // try {
+    //     const newEvent = await Event.find()
+    //     console.log(newEvent)
+    //     res.send(newEvent)
+    // } catch(err) {
+    //     res.send(err)
+    // }
+    //const newEvent = await Event.find();
+    return Event.find().lean();
 }
 
-module.exports.createEvent = async (req, res, next) => {
-    try {
-        const event = new Event({
-            title : req.body.title,
-            description : req.body.description
-        })
-        console.log(event)
-        const newEvent = await event.save();
-        res.json(newEvent);
-    } catch (error) {
-        res.send({message: error.message})
-    }
+module.exports.createEvent = async (body) => {
+    const event = new Event({
+        title : body.title,
+        description : body.description
+    })
+    return event.save();
 }
 
-module.exports.getEvent = async (req, res, next)=>{
-    try{
-        const event = await Event.findById(req.params.id)
-        console.log(event)
-        if(event == null) return res.json({message: "cannot find event"})
-        res.json(event)
-    } catch (err){
-        res.send(err);
-    }
+module.exports.getEvent = (id)=>{
+    return Event.findById(id);
 }
 
-module.exports.deleteEvent = async (req, res, next)=>{
-    try {
-        const eventIds = req.params.id.split(',');
-        // eventsIds.forEach((eventId)=>{
-           
-        // })
-        for(let i = 0; i < eventIds.length; i++){
-            await Event.findByIdAndRemove(eventIds[i])
-        }
-        res.json({message : "events got deleted"})
-    } catch (error) {
-        res.send(error);
+module.exports.deleteEvent = async (ids)=>{
+    
+    const eventIds = ids.split(',');
+    for(let i = 0; i < eventIds.length; i++){
+         await Event.findByIdAndRemove(eventIds[i])        
     }
+    return;
+
 }
 
-module.exports.updateEvent = async (req, res, next)=>{
-    try {
-        const event = await Event.findById(req.params.id)
-        if(req.body.title != null){
-            event.title = req.body.title
-        }
-        if(req.body.description != null){
-            event.description = req.body.description
-        }
-        const e1 = await event.save();
-        res.json(e1);
-        
-    } catch (error) {
-        res.send(error);
+module.exports.updateEvent = async (id, body)=>{
+    
+    const event = await Event.findById(id)
+    if (body.title != null) {
+        event.title = body.title
     }
+    if (body.description != null) {
+        event.description = body.description
+    }
+    return event.save();
+
 }
--- routes/events.js
+++ routes/events.js
@@ -5,21 +5,40 @@ var eventlist = require('../controllers/events');
 const event = require('../models/event');
 
 //get request at /events
-router.get('/', eventlist.eventListAll);
+router.get('/', (req, res, next) => {
+    
+    eventlist.eventListAll()
+    .then(events => res.json(events))
+    .catch(err => res.json(err))
+});
 
 //post request at /events for creating events
-router.post('/', eventlist.createEvent)
+router.post('/', (req, res, next) => {
+    eventlist.createEvent(req.body)
+    .then(event => res.json(event))
+    .catch(err => res.json({message : err.message}))
+})
 
 //finding event by passing id in params
-router.get('/:id', eventlist.getEvent)
+router.get('/:id', (req, res) => {
+    eventlist.getEvent(req.params.id)
+    .then(event => res.json(event))
+    .catch(err => res.json(err.message))
+})
 
 //delete rquest at /event/id
-router.delete('/:id', eventlist.deleteEvent);
+router.delete('/:id', (req, res) => {
+    eventlist.deleteEvent(req.params.id).then(() => res.json('event got deleted'))
+});
 
 
 
 //update request at /event
-router.patch('/:id', eventlist.updateEvent)
+router.patch('/:id', (req, res) => {
+    eventlist.updateEvent(req.params.id, req.body)
+    .then(event => res.json(event))
+    .catch(err => res.json(err))
+})
 
 
 module.exports = router
